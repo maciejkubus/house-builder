f3ca4c142aeff255d9450408eed5bf743415a309
Maciej Kubus <maciek.krol.zycia@gmail.com>
2024-03-18 16:01:29 +0100

    + basic

---
+++ builder.js
@@ -0,0 +1,4 @@
+(function() {
+	const canvas = document.querySelector('#house-builder-canvas');
+	console.log(canvas);
+})()
--- index.php
+++ index.php
@@ -5,8 +5,16 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>House builder</title>
 		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="builder.css">
 	</head>
 	<body>
+		<div id="house-builder">
+			<canvas id="house-builder-canvas">
+
+			</canvas>
+		</div>
+
 		
+		<script src="builder.js"></script>
 	</body>
 </html>
